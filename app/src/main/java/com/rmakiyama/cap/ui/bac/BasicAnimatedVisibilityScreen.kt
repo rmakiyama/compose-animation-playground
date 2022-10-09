a4a112cc5b2536b3e1d7b627ad2e542d8fd9454b
rmakiyama <rmakiyama.io@gmail.com>
2022-10-10 00:02:43 +0900

    add first step

--- app/src/main/java/com/rmakiyama/cap/ui/bac/BasicAnimatedVisibilityScreen.kt
+++ app/src/main/java/com/rmakiyama/cap/ui/bac/BasicAnimatedVisibilityScreen.kt
@@ -2,15 +2,35 @@
 
 package com.rmakiyama.cap.ui.bac
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement.spacedBy
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.twotone.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 import com.rmakiyama.cap.R
 import com.rmakiyama.cap.designsystem.component.CapTopAppBar
 
@@ -27,26 +47,74 @@ fun BasicAnimatedVisibilityRoute(
 fun BasicAnimatedVisibilityScreen(
     onBackClick: () -> Unit,
 ) {
+    val listState = rememberLazyListState()
+    val topAppBarCanScroll by remember {
+        derivedStateOf {
+            listState.layoutInfo.totalItemsCount > listState.layoutInfo.visibleItemsInfo.size
+        }
+    }
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(
+        canScroll = { topAppBarCanScroll }
+    )
     Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
             CapTopAppBar(
                 titleRes = R.string.prototype_title_bav,
                 navigationIcon = Icons.TwoTone.ArrowBack,
                 navigationIconContentDescription = "back to home",
                 onNavigationClick = onBackClick,
-                scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(),
+                scrollBehavior = scrollBehavior,
             )
         }
     ) { innerPadding ->
-        Column(
-            modifier = Modifier.padding(innerPadding),
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 16.dp),
+            state = listState,
+            verticalArrangement = spacedBy(24.dp),
+            contentPadding = innerPadding,
         ) {
-            FirstStep()
+            sampleItem { FirstStep() }
+        }
+    }
+}
+
+private fun LazyListScope.sampleItem(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit,
+) {
+    item {
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.primaryContainer)
+                .padding(horizontal = 16.dp, vertical = 24.dp)
+        ) {
+            content()
         }
     }
 }
 
 @Composable
 private fun FirstStep() {
-
+    var visible by remember { mutableStateOf(true) }
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Box(
+            modifier = Modifier.weight(1f),
+            contentAlignment = Alignment.Center,
+        ) {
+            this@Row.AnimatedVisibility(visible = visible) {
+                Text("Sample text.")
+            }
+        }
+        Switch(
+            checked = visible,
+            onCheckedChange = { visible = !visible },
+            modifier = Modifier.weight(1f),
+        )
+    }
 }
