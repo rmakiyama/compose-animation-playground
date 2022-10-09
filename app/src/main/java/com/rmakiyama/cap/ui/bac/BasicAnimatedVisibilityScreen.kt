7e1350ab5c002b7b00634c314894a43a288ec60f
rmakiyama <rmakiyama.io@gmail.com>
2022-10-10 00:15:07 +0900

    add TryMutableTransitionState

--- app/src/main/java/com/rmakiyama/cap/ui/bac/BasicAnimatedVisibilityScreen.kt
+++ app/src/main/java/com/rmakiyama/cap/ui/bac/BasicAnimatedVisibilityScreen.kt
@@ -3,9 +3,14 @@
 package com.rmakiyama.cap.ui.bac
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement.spacedBy
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -22,6 +27,7 @@ import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -33,6 +39,7 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import com.rmakiyama.cap.R
 import com.rmakiyama.cap.designsystem.component.CapTopAppBar
+import kotlinx.coroutines.delay
 
 @Composable
 fun BasicAnimatedVisibilityRoute(
@@ -77,6 +84,7 @@ fun BasicAnimatedVisibilityScreen(
             contentPadding = innerPadding,
         ) {
             sampleItem { FirstStep() }
+            sampleItem { TryMutableTransitionState() }
         }
     }
 }
@@ -115,6 +123,40 @@ private fun FirstStep() {
             checked = visible,
             onCheckedChange = { visible = !visible },
             modifier = Modifier.weight(1f),
+        )
+    }
+}
+
+@Composable
+private fun TryMutableTransitionState() {
+    val state = remember {
+        MutableTransitionState(false)
+    }
+
+    LaunchedEffect(Unit) {
+        delay(500L)
+        state.targetState = true
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .animateContentSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        AnimatedVisibility(
+            visibleState = state,
+            enter = fadeIn(animationSpec = tween(durationMillis = 1_000)),
+        ) {
+            Text(text = "Hello, world!")
+        }
+        Text(
+            text = "State: " + when {
+                state.isIdle && state.currentState -> "Visible"
+                !state.isIdle && state.currentState -> "Disappearing"
+                state.isIdle && !state.currentState -> "Invisible"
+                else -> "Appearing"
+            }
         )
     }
 }
