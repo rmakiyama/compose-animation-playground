a890c87ce50ac272981417ce2bc93bf998418a47
rmakiyama <rmakiyama.io@gmail.com>
2022-10-10 13:45:19 +0900

    fix modifier

--- app/src/main/java/com/rmakiyama/cap/ui/progress/FlickrLikeProgress.kt
+++ app/src/main/java/com/rmakiyama/cap/ui/progress/FlickrLikeProgress.kt
@@ -4,10 +4,8 @@ import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -20,6 +18,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
@@ -50,10 +49,7 @@ fun FlickrLikeProgress(
         reversed = true
     }
 
-    Row(
-        modifier = modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.Center,
-    ) {
+    Row(modifier = modifier) {
         Circle(
             color = Color.Red,
             modifier = Modifier
@@ -67,4 +63,10 @@ fun FlickrLikeProgress(
                 .zIndex(if (reversed) 1f else -1f),
         )
     }
+}
+
+@Preview
+@Composable
+private fun FlickrLikeProgressPreview() {
+    FlickrLikeProgress()
 }
--- app/src/main/java/com/rmakiyama/cap/ui/progress/ProgressScreen.kt
+++ app/src/main/java/com/rmakiyama/cap/ui/progress/ProgressScreen.kt
@@ -2,37 +2,18 @@
 
 package com.rmakiyama.cap.ui.progress
 
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.twotone.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
 import com.rmakiyama.cap.R
 import com.rmakiyama.cap.designsystem.component.CapTopAppBar
 import com.rmakiyama.cap.ui.extension.sampleItem
@@ -66,7 +47,9 @@ fun ProgressScreen(
             verticalArrangement = Arrangement.spacedBy(24.dp),
             contentPadding = innerPadding,
         ) {
-            sampleItem(backgroundColor = sampleBackgroundColor) { FlickrLikeProgress() }
+            sampleItem(backgroundColor = sampleBackgroundColor) {
+                FlickrLikeProgress()
+            }
         }
     }
 }
